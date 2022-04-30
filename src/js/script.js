cec80639dd6e3bd88eef77ddf8c1fc617b8d3722
Olga <boa906680@gmail.com>
2022-04-30 12:56:09 +0300

    add slider and tabs

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right-solid.png"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false,
+            }
+        }]
+    });
+});
+
+// const slider = tns({
+//     container: '.carousel__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     controls: false,
+//     nav: false
+// });
+
+// document.querySelector('.prev').addEventListener('click', function() {
+//     slider.goTo('prev');
+// });
+
+// document.querySelector('.next').addEventListener('click', function() {
+//     slider.goTo('next');
+// });
